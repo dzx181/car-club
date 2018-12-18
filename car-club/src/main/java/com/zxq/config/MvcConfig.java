a2552f1eeefa9ce8b5d0b82369bae4299340ec08
Deng Zhenxiang <dengzhenxiang@haylion.cn>
2018-12-18 09:07:58 +0800

    美化页面

--- car-club/src/main/java/com/zxq/config/MvcConfig.java
+++ car-club/src/main/java/com/zxq/config/MvcConfig.java
@@ -5,15 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+/**
+ * <p>
+ *  webmvc配置
+ * </p>
+ *
+ * @author Zeng XiaoQi
+ * @since 2018-12-08
+ */
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
-
+    /**
+     *  super.addInterceptors(registry);
+     *  静态资源: *.css , *.js
+     *  SpringBoot已经做好了静态资源映射
+     * @param registry
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        //super.addInterceptors(registry);
-        //静态资源: *.css , *.js
-        //SpringBoot已经做好了静态资源映射
         registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
                 .excludePathPatterns("/login", "/", "/toLogin", "/css/**", "/fonts/**", "/img/**", "/js/**","/laydate/**");
     }
--- car-club/src/main/java/com/zxq/dao/DocDao.java
+++ car-club/src/main/java/com/zxq/dao/DocDao.java
@@ -6,9 +6,24 @@ import com.zxq.entity.vo.DocVO;
 import java.util.List;
 
 public interface DocDao {
+    /**
+     * 上传文件
+     * @param docVO
+     * @return
+     */
     int fileUpload(DocVO docVO);
 
+    /**
+     * 查找文件
+     * @param doc
+     * @return
+     */
     List<Doc> selectFile(Doc doc);
 
+    /**
+     * 删除文件
+     * @param caption
+     * @return
+     */
     int deleteFile(List<Integer> caption);
 }
